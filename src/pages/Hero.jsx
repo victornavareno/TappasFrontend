f42d8868589afee4121f228b279f21c86294187e
victornavareno <vnavareo@alumnos.unex.es>
2025-04-27 10:54:19 +0200

    refactor: deleted tango files, fixed landing code

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -13,8 +13,10 @@ function Landing() {
         backgroundImage: "url('/backgrounds/TappasLandingBackground.png')",
       }}
     >
-      {/* Header */}
-      <header className="container mx-auto px-6 py-2">
+      {/* Header - Reducimos el padding vertical para que afecte menos */}
+      <header className="container mx-auto px-6 py-4">
+        {" "}
+        {/* Cambiado de py-8 a py-4 */}
         <div className="flex items-center">
           <img
             src="/logo_tappas.png"
@@ -24,20 +26,18 @@ function Landing() {
         </div>
       </header>
 
-      <main className="container mx-auto flex-1 flex flex-col items-center justify-center text-center px-6 mt-[-4rem]">
-        <h1 className="text-6xl md:text-8xl font-extrabold mb-6 italic">
+      {/* Contenido Principal - Ajustar para centrado perfecto */}
+      <main className="container mx-auto flex-1 flex flex-col items-center justify-center text-center px-6 pb-20">
+        <h1 className="text-6xl md:text-8xl italic font-extrabold mb-4">
           TAPPAS
         </h1>
-
-        <p className="text-xl md:text-3xl mb-10 max-w-2xl">
+        <p className="text-2xl md:text-3xl mb-0 max-w-2xl transform translate-y-[-1rem]">
           Elige tu ciudad y qué te apetece y te recomendamos el mejor sitio!
         </p>
-
-        <div className="flex flex-col sm:flex-row gap-4">
+        <div className="flex flex-col sm:flex-row gap-4 mt-8">
           <button className={`${buttonStyles} bg-white text-black`}>
             SABER MÁS
           </button>
-
           <button
             className={`${buttonStyles} bg-yellow-400 text-black`}
             onClick={() => navigate("/explorar")}
